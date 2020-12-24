1cac674484d98361cf0e50333731e8fdad19f025
Nick <nvkada@gmail.com>
2020-12-24 21:55:52 +0300

    files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Data_Mining
- 211220
--- final_LS_1_HW.py
+++ final_LS_1_HW.py
@@ -3,14 +3,6 @@ from pathlib import Path
 import time
 import simplejson as json
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
-}
-
-url = 'https://5ka.ru/api/v2/categories/'
-url_cat = 'https://5ka.ru/api/v2/special_offers/?categories=&ordering=&page=2&price_promo__gte=&price_promo__lte=&records_per_page=12&search=&store='
-
-
 class StatusCodeError(Exception):
 
     def __init__(self, txt):
@@ -22,12 +14,13 @@ class Parser5ka_special:
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
     }
 
+
     url_cat = 'https://5ka.ru/api/v2/special_offers/?categories=&ordering=&page=2&price_promo__gte=&price_promo__lte=&records_per_page=12&search=&store='
 
     def __init__(self, start_url):
         self.start_url = start_url
 
-    def _get_response(url, **kwargs):
+    def _get_response(self, url, **kwargs):
         while True:
             try:
                 response = requests.get(url, **kwargs)
@@ -41,7 +34,7 @@ class Parser5ka_special:
 
     def parse_cat(self, start_url):
 
-        response = self._get_response(start_url, headers=headers)
+        response = self._get_response(start_url, headers=self.headers)
 
         data: dict = response.json()
 
@@ -56,23 +49,25 @@ class Parser5ka_special:
         list_urls_to_pars = {}
         cats  = self.parse_cat(self.start_url)
         for i in cats:
-            new_url = url_cat.replace("categories=", f"categories={i}")
+            new_url = self.url_cat.replace("categories=", f"categories={i}")
             list_urls_to_pars[f"{cats[i]}_код_{i}"] = new_url
         return list_urls_to_pars
 
 
-    def parse(url):
+    def parse(self, url):
         while url:
-            response = self._get_response(url, headers=headers)
+            response = self._get_response(url, headers=self.headers)
             data: dict = response.json()
             url = data['next']
             yield data.get('results', [])
 
-    urslss = 'https://5ka.ru/api/v2/special_offers/?categories=902&ordering=&page=2&price_promo__gte=&price_promo__lte=&records_per_page=12&search=&store='
 
+    def run(self):
+        for i in self.constract_url():
+            with open(f'{i}.json', 'w',  encoding='UTF-8') as file:
+                    file.write(i)
+                    json.dump(self.parse(self.constract_url()[i]), file, ensure_ascii=False, iterable_as_array=True)
 
-    for i in constract_url():
-        with open(f'{i}.json', 'w',  encoding='UTF-8') as file:
-            file.write(i)
-            json.dump(parse(constract_url()[i]), file, ensure_ascii=False, iterable_as_array=True)
-
+if __name__ == '__main__':
+    parser  = Parser5ka_special("https://5ka.ru/api/v2/categories/")
+    parser.run()
